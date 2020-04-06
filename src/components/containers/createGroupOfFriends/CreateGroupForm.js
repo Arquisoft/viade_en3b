43f54f0e7164a0fca44b7c9c4681c6cdf4e5b832
Carmen <uo264045@uniovi.es>
2020-04-06 23:27:23 +0200

    I think ui is done

--- src/components/containers/createGroupOfFriends/CreateGroupForm.js
+++ src/components/containers/createGroupOfFriends/CreateGroupForm.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import { makeStyles } from '@material-ui/core/styles';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 export class CreateGroupForm extends Component{
 
@@ -11,20 +17,62 @@ export class CreateGroupForm extends Component{
         };
     }
 
-    changeName = (newName) => {
-        this.setState({name : newName});
-    }
+    useStyles = makeStyles((theme) => ({
+        root: {
+          display: 'flex',
+        },
+        formControl: {
+          margin: theme.spacing(3),
+        },
+      }));
 
-    changeFriends = (newFriends) => {
-        this.setState({friends : newFriends});
+    changeName = (event) => {
+        this.setState({name : event.target.value});
     }
 
     render(){
-        const { name, friends } = this.state;
-        const values = { name, friends };
+
+        const classes = this.useStyles();
+        const [checked, setChecked] = React.useState(false);
+
+        const changeFriends = (event) => {
+            this.setState({friends : event.target.value}); 
+            setChecked(event.target.checked);
+        }
+
+        const childrenTrimmed = (item) =>            
+            <Checkbox
+                checked={checked}
+                value = {this.state.friends}
+                label = {`${item}`.split('.')[0].split('//')[1]}
+                onChange={changeFriends}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+            />;
+
         return(
-            "this str is to get rid of the error for commiting, dont worry"
+            <React.Fragment>
+                <h2>
+                    Create a new group of friends
+                </h2>
+                <form>
+                    <label>
+                        Name: 
+                        <input type="text" value={this.state.name} onChange={this.changeName} />
+                    </label>
+                    <FormControl component="fieldset" className={classes.formControl}>
+                        <FormLabel component="legend">Friends in your POD</FormLabel>
+                        <FormGroup>
+                            <FormControlLabel
+                                friends = {childrenTrimmed}
+                            />          
+                        </FormGroup>
+                    </FormControl>
+                </form>
+            </React.Fragment>
         );
     }
 
+    storeGroup = () => {
+        //storeInAddressBook(this.name, this.friends);
+    }
 }
