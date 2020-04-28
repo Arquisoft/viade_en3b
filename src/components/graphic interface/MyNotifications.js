8f95ddcdf8770369e452323301868158afed47da
Carmen <uo264045@uniovi.es>
2020-04-28 11:21:45 +0200

    interface changes (access to the cache, adding buttons...)

--- src/components/graphic interface/MyNotifications.js
+++ src/components/graphic interface/MyNotifications.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { MenuItem, Paper, Popper, Grow, MenuList, Typography } from '@material-ui/core';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+import cache from "../../cache/RoutesChache.js";
 
 const MyNotifications = () => {
     const [open, setOpen] = React.useState(false);
@@ -12,8 +15,7 @@ const MyNotifications = () => {
         setOpen((prevOpen) => !prevOpen);
     };
 
-
-    const notifications = []; // we will get notifications here somehow
+    const notifications = cache.getNotificationFromCache();
     const showNotifications = () => {
         if(notifications.length < 1) {
             return <MenuItem>
@@ -23,9 +25,23 @@ const MyNotifications = () => {
             notifications.forEach(notification => {
                 return <MenuItem>
                     <Typography>{notification}</Typography>
+                    <Button onClick={acceptNotification(notification)}>
+                        <CheckIcon />
+                    </Button>
+                    <Button onClick={declineNotification(notification)}>
+                        <CloseIcon />
+                    </Button>
                 </MenuItem>
             });
         }
+    };
+
+    const acceptNotification = (notification) => {
+        cache.addSharedRoute(notification);
+    };
+
+    const declineNotification = (notification) => {
+        cache.removeNotification(notification);
     };
 
     return (
